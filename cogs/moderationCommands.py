aa96b49282be7671c8c37d84dbd1a4cda44ac415
Pickle423 <89610484+Pickle423@users.noreply.github.com>
2021-10-04 21:53:29 -0400

    Finished moderation commands, added muting feature.

--- cogs/moderationCommands.py
+++ cogs/moderationCommands.py
@@ -1,5 +1,12 @@
 import discord
 from discord.ext import commands
+global muted
+my_file = open("muted.txt", "r")
+muted = my_file.readlines()
+my_file.close()
+print(muted)
+intents = discord.Intents.default()
+intents.members = True
 
 #Moderation Cog
 class Moderation(commands.Cog):
@@ -8,6 +15,15 @@ class Moderation(commands.Cog):
     async def cog_check(self, ctx):
         #Check if user has admin role
         return ctx.author.guild_permissions.manage_messages
+    
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        global muted
+        joineduser = str(member)
+        for user in muted:
+            if user == joineduser:
+                role = discord.utils.get(member.guild.roles, name='Muted')
+                await member.add_roles(role)
 
     @commands.command()
     @commands.has_role("Operations Command")
@@ -34,6 +50,38 @@ class Moderation(commands.Cog):
                 await ctx.guild.unban(user)
                 await ctx.send(f'Unbanned {user.name}#{user.discriminator}')
                 return
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def mute(self, ctx, *, member : discord.Member):
+        global muted
+        print(muted)
+        guild = ctx.guild
+        role = discord.utils.get(ctx.guild.roles, name='Muted')
+        user = member
+        await user.add_roles(role)
+        user = str(user)
+        print(user)
+        muted.append(user)
+        with open("muted.txt", "w") as f:
+            for element in muted:
+                f.write(element)
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def unmute(self, ctx, *, member : discord.Member):
+        global muted
+        guild = ctx.guild
+        role = discord.utils.get(ctx.guild.roles, name='Muted')
+        user = member
+        await user.remove_roles(role)
+        user = str(user)
+        print(user)
+        print(f'before removal: {muted}')
+        muted.remove(user)
+        print(f'post removal: {muted}')
+        with open("muted.txt", "w") as f:
+            for element in muted:
+                f.write(element)
 
 def setup(client):
     client.add_cog(Moderation(client))
