0212f27d151fe0bbb006a7d2a9ff6ce42702aa09
in <Pickle423423@gmail.com>
2021-10-19 18:01:13 -0400

    updated channel name

--- cogs/gifFilter.py
+++ cogs/gifFilter.py
@@ -7,7 +7,7 @@ class gifFilter(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        exception_channels = [discord.utils.get(message.guild.text_channels, name="voice-chat"),discord.utils.get(message.guild.text_channels, name="nsfw-memes-no-porn"),discord.utils.get(message.guild.text_channels, name="bot-commands")]
+        exception_channels = [discord.utils.get(message.guild.text_channels, name="voice-chat"),discord.utils.get(message.guild.text_channels, name="random"),discord.utils.get(message.guild.text_channels, name="bot-commands")]
         if message.channel not in exception_channels:
             not_allow = message.content
             if "https://tenor.com" in not_allow or "https://media.tenor.co" in not_allow or "https://c.tenor.com" in not_allow:
