0b54f6e40c5d6ddfee77a8768658fcf3a59aab0c
in <Pickle423423@gmail.com>
2021-11-14 11:21:09 -0500

    No Fun Allowed

--- cogs/miscCommands.py
+++ cogs/miscCommands.py
@@ -9,12 +9,7 @@ class miscCommands(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
-    
-    #Linux Funny
-    @commands.command(aliases=["LinuxOS", "GNU/Linux", "GNU", "Linux/GNU", "unfunny"])
-    async def Linux(self, ctx):
-        await ctx.send(f"I’d just like to interject for a moment. What you’re refering to as Linux, is in fact, GNU/Linux, or as I’ve recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX. \n \nMany computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called Linux, and many of its users are not aware that it is basically the GNU system, developed by the GNU Project. \n \nThere really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine’s resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called Linux distributions are really distributions of GNU/Linux!")
-
+ 
     #Sus
     @commands.command()
     async def sus(self, ctx):
@@ -60,9 +55,9 @@ class miscCommands(commands.Cog):
     async def version(self, context):
 
         mainEmbed = nextcord.Embed(title="Kakapo Version Notes", description="SESO's Multi-Use Discord Bot", color=0x0E8643)
-        mainEmbed.add_field(name="Changes:", value="Music can now be searched for via text, music quality of life additions.")
-        mainEmbed.add_field(name="Version Code:", value="v0.9.3", inline=False)
-        mainEmbed.add_field(name="Date Released:", value="October 21, 2021", inline=False)
+        mainEmbed.add_field(name="Changes:", value="An Imposter Among Us released, music system improved.")
+        mainEmbed.add_field(name="Version Code:", value="v0.9.6", inline=False)
+        mainEmbed.add_field(name="Date Released:", value="November 13th, 2021", inline=False)
         mainEmbed.set_footer(text="Kakapo written by Pickle423#0408, Fletch#0617, Dildo Sagbag#8107.")
 
         await context.message.channel.send(embed=mainEmbed)
