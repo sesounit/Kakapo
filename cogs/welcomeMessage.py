436e16047dad1863df51e66d770d71b72db3b304
in <Pickle423423@gmail.com>
2022-04-16 14:46:49 -0400

    updated link in welcome message.

--- cogs/welcomeMessage.py
+++ cogs/welcomeMessage.py
@@ -9,7 +9,7 @@ class WelcomeMessage(commands.Cog):
     async def on_member_join(self, member):
         global welcomemessagechannel
         channel = nextcord.utils.get(member.guild.text_channels, name="lobby")
-        await channel.send(f"{member.mention}, welcome to [S.E.S.O.] Casual Milsim. \n \n See #react-for-roles to choose between Operative, a regular member, or Combat Service Support, a spectator. You will not see most channels without a role.  \n \n If you are new to Arma 3, see the New Operative Guide to get started: \n  https://wiki.sesounit.org/tutorials_guides:operator_guide \n \n If you want to try us out, ask <@!877973354164936734> or <@!877973354164936734> for our primer mission. The primer mission is our custom scenario showcasing our play style.")
+        await channel.send(f"{member.mention}, welcome to [S.E.S.O.] Casual Milsim. \n \n See #react-for-roles to choose between Operative, a regular member, or Combat Service Support, a spectator. You will not see most channels without a role.  \n \n If you are new to Arma 3, see the New Operative Guide to get started: \n  https://wiki.sesounit.org/guides/new_operative \n \n If you want to try us out, ask <@!877973354164936734> or <@!877973354164936734> for our primer mission. The primer mission is our custom scenario showcasing our play style.")
 
 def setup(client):
     client.add_cog(WelcomeMessage(client))
