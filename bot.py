8b825043b0b16f998b34f9088bc949bd29882c8a
Uncle Sagbag <frederick.alexander@tutanota.com>
2023-02-23 12:10:35 -0500

    Added intents and debug helpers

--- bot.py
+++ bot.py
@@ -2,14 +2,30 @@
 import nextcord, os, logging
 from dotenv import load_dotenv
 from nextcord.ext import commands, tasks
-intents = nextcord.Intents.default()
-intents.members = True
 # is a .env file inside the folder to leave the token for the bot outside the git
 load_dotenv()
 
 # Debug Helpers
-## Logging
-## logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+debug = bool(os.getenv('DEBUG'))
+if debug:
+    print('Setting DEBUG log level')
+    logger.setLevel(logging.debug)
+
+# Setup intents
+intents = nextcord.Intents.default()
+intents.guilds = True
+intents.message_content = True
+intents.reactions = True
+intents.members = True
 
 # bot commands have prefix ! so all messages start with ! will trigger the bot commands
 client = commands.Bot(command_prefix='!', intents=intents, help_command = None, case_insensitive=True)
@@ -35,7 +51,7 @@ async def unload(ctx, extension):
 @client.command()
 @commands.has_permissions(administrator=True)
 async def reload(ctx, extension):
-        if extension == "all" or extension == "All":
+        if extension.lower() == "all":
             for filename in os.listdir('./cogs'):
                 try:
                     if filename.endswith('.py'):
