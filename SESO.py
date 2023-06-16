d31758c3cb5f34d00bb42db7ddd7e5ff95e74422
Uncle Sagbag <frederick.alexander@tutanota.com>
2023-06-16 04:14:43 +0000

    Uploaded latest changes to reddit autobot

--- SESO.py
+++ SESO.py
@@ -14,71 +14,56 @@ print(reddit.user.me())
 
 
 s = """
-**See our website for screenshots, briefings, wiki, merchandise, and more.**
-https://www.sesounit.org/
+# Variety with Common Sense Realism
+SESO was established in 2018 as a unit focused on immersion, variety, and common sense realism for a complete Arma 3 experience. We design operations that are fun to play, easy to join, and asks as little as possible from you. Our casual playstyle is tailored towards having fun, but we always play to complete the objective. There is no required experience, minimum age, DLCs, or “basic training” in order to participate in our operations.
 
 ***
-# Schedule
+## Schedule
 We focus on on EST (Eastern United States) timezone. We have players from PST, MST, CST, EST, BST, CEST, and MSK.
 
-Operation | Saturday | Sunday
-:--|:--|:--
-Operation Angola | 1800 EST |
-Operation Osiris | | 1800 EST
+Our main days are 18:00 EST every Saturday. Occasionally, we may run operations at 18:00 EST on Sunday or Friday.
+ 
+## Roles, not Ranks
+We do not privilege players with medals or ranks just because they have been with a unit for a long time. Roles are given to those who ask for it. Our structure allows anyone choose any role, whether it be squad leader, sniper, UAV operator, etc. as we trust you can fulfill the responsibility. Players do not have to commit to a particular role and are open to switch any time.
 
-***
-# S.E.S.O.
-SESO was established in 2018 with a focus on variety and story. We design 3-month long operations that are fun to play, easy to join, and contribute to the [SESO canon](https://wiki.sesounit.org/canon:list). The casual playstyle is tailored to allow one to join an operation as soon as possible. There is no required experience, minimum age, or test.
+## Stories that Mean Something
+Every operation has a story, whether it be tied to our unit's canon or is it's own thing. The story makes the setting and operations change the story. Players fight for a reason, for the story, and every objective affects the story. We avoid generic "you're the good guys, they're the bad guys" stories.
 
-## Rankless Structure
-
-We do not hold any ranks, in-game or out. Our structure allows anyone choose any role they wish, whether it be squad leader, sniper, UAV operator, etc. as long one fulfills their role. Players do not have to commit to a particular role and are open to switch roles every week.
-
-## Canon
-
-Operations set in the S.E.S.O. canon that span 90+ years. This means our operations can be set in any time period and setting, granting enormous variety. We have operated during the invasion of Czechoslovakia ([Sudeten Crisis, 1938](https://www.sesounit.org/timeline/operation-sudeten-crisis/)), the Vietnam War ([Operation Burning Buddhist, 1965](https://www.sesounit.org/timeline/operation-burning-buddhist/)), the Chernobyl Exclusion Zone ([Operation Green Diamond, 1988](https://www.sesounit.org/timeline/operation-green-diamond/)), and the Donbass War ([Crimean Crisis, 2014](https://www.sesounit.org/timeline/operation-crimean-crisis/)). We also operate in fictional settings that are based off historical events such as a Yugoslavian civil war set on Fapovo Islands ([Operation Openhouse, 1992](https://www.sesounit.org/timeline/operation-openhouse/)), and a southern African civil war on Isla Duala ([Isla Duala Civil War, 2000](https://www.sesounit.org/timeline/operation-isla-duala/)).
+And if you don't care about the story, you can avoid it.
 
 ## Active Community
 
-We play Arma 3 scenarios and other games every night. If you are looking for a community of friends, we are always welcoming.
+We play games every night. If you are looking for a community of friends, we are always welcoming.
 
 ***
+# Media
+All media is linked and provided on the [S.E.S.O. Youtube Channel](https://www.youtube.com/channel/UC5iMX4ubNxfiFaG2fbAOKOg/featured).
 
-#Media
-All media is linked and provided on the [S.E.S.O. Youtube Channel](https://www.youtube.com/channel/UC5iMX4ubNxfiFaG2fbAOKOg/featured)
+## Recommended S.E.S.O. videos:
 
-##Recommended S.E.S.O. videos:
+[Operation Green Diamond | 5th Engagement](https://www.youtube.com/watch?v=yiBY-arKDqI), full operation footage from an operation set in the Chernobyl Exclusion Zone.
 
-[Operation Green Diamond | 5th Engagement](https://youtu.be/yiBY-arKDqI), full engagement footage from an operation set in the Chernobyl Exclusion Zone.
+[Operation Osiris Trailer](https://youtu.be/u7_2CUmIuQ8), a trailer for a campaign set in a post apocalyptic Chernarus.
 
-[Operation Burning Buddhist Trailer](https://www.youtube.com/watch?v=d8tj5IT-fi4), a trailer for our 5th operation set in the Vietnam War
+[SESO in: "The Reconstruction of Mankind"](https://youtu.be/6i9VOpU5CWk), a collection of clips from the community showcasing us.
 
-[Operation Burning Buddhist | 2nd Engagement](https://www.youtube.com/watch?v=RlG4znKKK_E), full engagement footage of an engagement.
+[Operation Perun | Final Engagement](https://www.youtube.com/watch?v=wnNviA-t7UU), a personal favorite. A 3 month campaign that ended with a bang.
 
 [Operation Trebuchet | Side Campaign](https://www.youtube.com/watch?v=fLH6BiG3RD8), a collage from our Operation Trebuchet campaign set in the Halo universe.
 
-[Operation Perun | Final Engagement](https://youtu.be/wnNviA-t7UU), a personal favorite. A 3 month operation that ended with an explosive engagement.
-
-[SESO Arma funny moments | Compilation of Silliness](https://youtu.be/sqokKzQn9vc). A perfect example of our casual attitude.
+[SESO Arma funny moments | Compilation of Silliness](https://www.youtube.com/watch?v=sqokKzQn9vc), another perfect example of our casual attitude.
 
 ***
+# How Do I Join S.E.S.O?
+First, join our discord: https://discord.gg/umGXdNgs8A
 
-# How to Join S.E.S.O.
-Follow the [New Operator Guide](https://wiki.sesounit.org/guides/new_operative) at our wiki.
+Second, if you are new to Arma, check out our New Operative Guide here: https://wiki.sesounit.org/guides/new_operative
 
-If you have any questions, please join us in our Discord:
-https://discord.gg/AVE9Gvq97C
-
-***"""
-
-# subreddit = reddit.subreddit('FindAUnit')
-#submission = subreddit.submit(
-#	'[NA][Recruiting][Casual][A3] S.E.S.O. | Private Military Company',
-#	selftext=s,)
-
+***
+"""
 subreddit = reddit.subreddit('FindAUnit')
 submission = subreddit.submit_image(
-	'[NA][Recruiting][Casual][A3] S.E.S.O. | Private Military Company',
-	'result.jpg'
+	'[NA][Recruiting][Casual][A3] S.E.S.O. | Variety with Common Sense Realism',
+	'/home/arma3server/bot/Kakapo/result.jpg'
 )
 submission.reply(s)
